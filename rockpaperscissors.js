038d0b0a7f9f9e6a5e9c2d138ba1d32fdc54c809
lukegrob <178822399+lukegrob@users.noreply.github.com>
2024-09-04 15:14:28 +0100

    Add getHumanChoice()

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,21 +4,46 @@
 
 */
 function getComputerChoice()
-{   let result;
+{   
     let compChoice = Math.ceil(Math.random()*3);
     //console.log(compChoice);
     switch(compChoice)
     {
         case(1):
-            result = "rock";
+            compChoice = "rock";
             break;
         case(2):
-            result = "paper";
+            compChoice = "paper";
             break;
         case(3):
-            result = "scissors";
+            compChoice = "scissors";
     }
-    return result;
+    return compChoice;
+}
+
+function getHumanChoice()
+{
+    let humanChoice = prompt("Make your choice: Rock, Paper, or Scissors","").trim().toLowerCase();
+    
+    switch(humanChoice)
+    {
+        case("rock"):
+        case("paper"):
+        case("scissors"):
+            break;
+        default:
+            humanChoice = "paper";
+    }
+
+    return humanChoice;
 }
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
+
+
+
+
+
+
+
