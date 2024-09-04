1a57df340f55b6b4a4b9d43fc3e604adb1f3df96
lukegrob <178822399+lukegrob@users.noreply.github.com>
2024-09-04 14:57:21 +0100

    Add getComputerChoice()

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+/*---PSEUDOCODE---
+
+
+
+*/
+function getComputerChoice()
+{   let result;
+    let compChoice = Math.ceil(Math.random()*3);
+    //console.log(compChoice);
+    switch(compChoice)
+    {
+        case(1):
+            result = "rock";
+            break;
+        case(2):
+            result = "paper";
+            break;
+        case(3):
+            result = "scissors";
+    }
+    return result;
+}
+
+console.log(getComputerChoice());
